6837dc1db4a687bcfded77e336871c9ef3663d13
cosminadudas <cosmina.dudas@yahoo.ro>
2022-01-16 15:26:10 +0200

    adding, removing and moving an element using JS

--- app.js
+++ app.js
@@ -9,3 +9,33 @@ anchorElement.href = "https://google.com";
 // selecting the first element that matches the condition
 // document.querySelector('a');
 // document.querySelector('#external-link');
+
+//* ADD AN ELEMENT
+// 1. Create the new element
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com'
+newAnchorElement.textContent = ' This leads to Google!'
+
+// 2. Get access to the parent element that should hold the new element
+let firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content.
+firstParagraph.append(newAnchorElement);
+
+
+//! REMOVE ELEMENTS
+// 1. Select the element that should be removed
+let firstH1Element = document.querySelector('h1');
+
+// 2. Remove it!
+// 2.1 First way to remove elements
+firstH1Element.remove();
+
+// 2.2 Second way to remove elements - for older browsers
+// firstH1Element.parentElement.removeChild(firstH1Element);
+
+
+//& MOVE ELEMENTS
+firstParagraph.parentElement.append(firstParagraph);
+
+
